e5cb0c392a7ea04d79113ec86876013a64e0c9e7
grandduke-onix <106667064+grandduke-onix@users.noreply.github.com>
2023-02-17 16:21:50 +0100

    designed the main carousel

--- src/scenes/home/index.jsx
+++ src/scenes/home/index.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import MainCarousel from "./MainCarousel";
 
 const Home = function () {
-	return <div>Home</div>;
+	return (
+		<div className="home">
+			<MainCarousel />
+		</div>
+	);
 };
 
 export default Home;
